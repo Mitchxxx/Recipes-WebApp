358551a9b4b34fb75d79b388c18529105742c05f
Mitchxxx <megboko@gmail.com>
2025-08-07 12:21:04 +0100

    Integrated Auth0 and a Navbar for login

--- recipes-web/src/App.js
+++ recipes-web/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Recipe from './Recipe';
+import Navbar from './Navbar';
 
 class App extends React.Component {
   constructor(props) {
@@ -20,7 +21,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        {this.state.recipes.map((recipe, index) => (
+        <Navbar />
+          {this.state.recipes.map((recipe, index) => (
           <Recipe recipe={recipe} key={index} />
         ))}
       </div>
--- recipes-web/src/index.js
+++ recipes-web/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+  domain="dev-fma3b265414e4z6v.uk.auth0.com"
+  clientId="6q8kqa2CL00rp4jdnHvj8Zy4p0mjnnDr"
+  redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
